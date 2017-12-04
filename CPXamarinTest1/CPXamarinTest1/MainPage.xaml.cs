cd84bc5fc57d6446999a89cc06eadd293911e8f3
shade86 <31279365+shade86@users.noreply.github.com>
2017-12-04 18:52:10 +0100

    Add project files.

---
+++ CPXamarinTest1/CPXamarinTest1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CPXamarinTest1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string text = TextBox.Text;
+            Label1.Text = "Hello " + text;
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
